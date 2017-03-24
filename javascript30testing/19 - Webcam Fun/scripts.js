565dba380a56ac0da71dd2232de31e534ace85bb
aldy120 <aldy120@yahoo.com.tw>
2017-03-24 14:18:18 +0800

    test camera

--- javascript30testing/19 - Webcam Fun/scripts.js
+++ javascript30testing/19 - Webcam Fun/scripts.js
@@ -11,7 +11,21 @@ function getVideo() {
       // HTML5 video has more functional, but it need a URL for src
       video.src = window.URL.createObjectURL(localMediaStream);
       video.play();
+    })
+    .catch(err => {
+      console.error('Oh No!', err);
     });
-    
+}
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  console.log(width, height);
+
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 20);
 }
